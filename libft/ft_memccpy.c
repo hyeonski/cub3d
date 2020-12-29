9c4815a0f0b46711f8df35cd6bdde941957e343b
hyeonski <hyeonski@student.42seoul.kr>
2020-12-29 21:40:43 +0900

    libft norm checked

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: hyeonski <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 14:15:38 by hyeonski          #+#    #+#             */
-/*   Updated: 2020/10/19 16:42:09 by hyeonski         ###   ########.fr       */
+/*   Updated: 2020/12/29 21:39:44 by hyeonski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void				*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	unsigned char	*result;
 	unsigned char	*source;
-	size_t	i;
+	size_t			i;
 
 	i = 0;
 	if (dest == NULL && src == NULL)
